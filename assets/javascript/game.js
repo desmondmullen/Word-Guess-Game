ea2caefc27d713ca9ceedff09807edf4e693c7f8
Desmond Mullen <dsm@busylittlestudios.com>
2018-12-14 12:02:12 -0500

    mostly works mechanically

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,68 @@
 // user: press key to start(listen for key)
-// comp: pick random word from array
-// display number of underscores for word
 // loop this:
-//     user: presses key(listen)
 //     comp: check that key is a letter.If no do nothing, else:
 //           check if key is in solution.If yes, tick sound and display in solution section, else check if letter has already been typed.If so, beep, else tick sound and display in letters guessed section.
 //     comp: check to see if solution has been completed, check to see if guessed letters has reached limit.Act accordingly if either of those happens.
 
-const allWordsToGuess = ["James", "Ann", "Vivian", "William", "Charles", "Elizabeth", "Charlotte", "Katherine", "Arthur", "Thomas"];
-let theWins = "0";
-let theGuessesRemaining = "5";
-var theLettersGuessed = [];
+const allWordsToGuess = ["Mary", "Helen", "Dorothy", "Margaret", "Ruth", "John", "William", "James", "Robert", "Charles"]; //1918-1919 top-five baby girl and top-five baby boy names
+const allTheValidGuesses = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let theWins = 0;
+let theGuessesRemaining = 0;
+let theLettersGuessedArray = [];
 let theGuess = "";
 let theWordToGuessArray = [];
-let theLetterThatMatchArray = [];
-let theDisplayArray = "_";
+let theLettersThatMatchArray = [];
+let randomMax = allWordsToGuess.length - 1 //minus 1 to make it zero-based
+let randomMin = 0
 
-// generate random number
-let randomNumber = function(min, max) {
-        return Math.floor(Math.random() * (+max - +min)) + +min;
-    }
-    // pick the word based on the random number
-let theWordToGuess = allWordsToGuess[randomNumber(0, 9)];
+// pick the word based on a random number
+let theWordToGuess = allWordsToGuess[Math.floor(Math.random() * (+randomMax - +randomMin)) + +randomMin];
+theGuessesRemaining = theWordToGuess.length + 2;
 // put the word into an array
-// theWordToGuessArray = theWordToGuess.split("")
-// join is opposite of split
+theWordToGuessArray = theWordToGuess.split("")
 for (var count = 0; count < theWordToGuess.length; count++) {
-    theWordToGuessArray.push(theWordToGuess.charAt(count));
-    theLetterThatMatchArray.push("_");
+    theLettersThatMatchArray.push("_");
 }
-console.log(theWordToGuessArray);
-console.log(theLetterThatMatchArray);
-
-// make the initial display of underscores
-for (let theCount = 1; theCount < theWordToGuess.length; theCount++) {
-    theDisplayArray = theDisplayArray + " _";
-}
-document.write(theDisplayArray);
 console.log(theWordToGuess);
-console.log(theDisplayArray);
+console.log(theLettersThatMatchArray.join(" "));
 
-// play the game
-for (let theCount = theGuessesRemaining; theCount >= 0; theCount--) {
+while (theGuessesRemaining > 0) {
     theGuess = (prompt("enter a letter to see if it's part of the word"));
-    theLettersGuessed.push(theGuess)
-    if (theWordToGuess.includes(theGuess)) {
-        alert(theGuess + " is in the word!");
-        // redo the display
-        for (i = 0; i < theWordToGuess.length; i++) {
-            //if theGuess is in theWordToGuessArray position[i] then splice;
-            if (theWordToGuessArray[i] == theGuess) {
-                theLetterThatMatchArray.splice(i, 1, theGuess);
+    // if it is not a letter then we will do nothing (maybe beep?)
+    if (!allTheValidGuesses.includes(theGuess) || theGuess.length !== 1) {
+        // beep or something?
+    } else {
+        theLettersGuessedArray.push(theGuess);
+        if (theWordToGuess.toLowerCase().includes(theGuess.toLowerCase())) {
+            // redo the display
+            for (i = 0; i < theWordToGuess.length; i++) {
+                // if theGuess is in theWordToGuessArray position[i] then splice
+                if (theWordToGuessArray[i].toLowerCase() == theGuess.toLowerCase()) {
+                    //check if the letter in theWordToGuessArray is capital and capitalize the spice if so
+                    if (theWordToGuessArray[i] === theGuess.toLowerCase()) {
+                        theLettersThatMatchArray.splice(i, 1, theGuess.toLowerCase());
+                    } else {
+                        theLettersThatMatchArray.splice(i, 1, theGuess.toUpperCase());
+                    }
+
+                    // theLettersThatMatchArray.splice(i, 1, theGuess);
+                }
             }
+            // then see if the word has been completed
+            if (!theLettersThatMatchArray.includes("_")) {
+                alert("You won!");
+                break;
+            }
+            console.log(theLettersThatMatchArray.join(" "));
+            //if the guess is wrong the we decrement the guesses remaining
+        } else {
+            theGuessesRemaining = theGuessesRemaining - 1;
         }
-        theDisplayString =
-            console.log(theLetterThatMatchArray);
+        console.log("Guesses remaining: " + theGuessesRemaining);
     }
-    console.log(theLettersGuessed);
 }
+if (theGuessesRemaining == 0) {
+    console.log("You ran out of guesses.");
+}
+console.log(theLettersGuessedArray.join(", "));
+document.write(theLettersThatMatchArray.join(" ") + " / your letters guessed: " + theLettersGuessedArray.join(", "));
