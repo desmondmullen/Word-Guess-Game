b9a319480b1f678f6445ce250bbfa4e3640509a0
Desmond Mullen <dsm@busylittlestudios.com>
2018-12-14 14:48:58 -0500

    mostly works mechanically

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,8 @@
 //           check if key is in solution.If yes, tick sound and display in solution section, else check if letter has already been typed.If so, beep, else tick sound and display in letters guessed section.
 //     comp: check to see if solution has been completed, check to see if guessed letters has reached limit.Act accordingly if either of those happens.
 
-const allWordsToGuess = ["Mary", "Helen", "Dorothy", "Margaret", "Ruth", "John", "William", "James", "Robert", "Charles"]; //1918-1919 top-five baby girl and top-five baby boy names
+const allWordsReset = ["Mary", "Helen", "Dorothy", "Margaret", "Ruth", "John", "William", "James", "Robert", "Charles"]; //1918-1919 top-five baby girl and top-five baby boy names
+var allWordsToGuess = allWordsReset;
 const allTheValidGuesses = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let theWins = 0;
 let theGuessesRemaining = 0;
@@ -14,63 +15,80 @@ let theWordToGuessArray = [];
 let theLettersThatMatchArray = [];
 let randomMax = allWordsToGuess.length - 1 //minus 1 to make it zero-based
 let randomMin = 0
-let theFinalMessage = ""
+let theMessage = ""
 
-// pick the word based on a random number
-let theWordToGuess = allWordsToGuess[Math.floor(Math.random() * (+randomMax - +randomMin)) + +randomMin];
-theGuessesRemaining = theWordToGuess.length + 2;
-// put the word into an array
-theWordToGuessArray = theWordToGuess.split("")
-for (var count = 0; count < theWordToGuess.length; count++) {
-    theLettersThatMatchArray.push("_");
+function resetGame() {
+    allWordsToGuess = allWordsReset;
+    theWins = 0;
+    playGame();
 }
-console.log(theWordToGuess);
-console.log(theLettersThatMatchArray.join(" "));
 
-while (theGuessesRemaining > 0) {
-    theGuess = (prompt("Fill in the blanks to guess this " + theWordToGuess.length + " letter name: " + theLettersThatMatchArray.join(" ") + "\nYou have guessed these letters so far: " + theLettersGuessedArray.join(", ") + "\nYou have " + theGuessesRemaining + " guesses left. (Correct guesses don't count against you.)\nEnter your guess here and press [return]"));
-    if (theGuess == null) {
-        document.write("Reload this page to play again!")
-        break;
+function playAgain() {
+    playGame();
+}
+
+function playGame() {
+    // pick the word based on a random number
+    let theWordToGuess = allWordsToGuess[Math.floor(Math.random() * (+randomMax - +randomMin)) + +randomMin];
+    theGuessesRemaining = theWordToGuess.length + 2;
+    // put the word into an array
+    theWordToGuessArray = theWordToGuess.split("")
+    for (var count = 0; count < theWordToGuess.length; count++) {
+        theLettersThatMatchArray.push("_");
     }
+    console.log(theWordToGuess);
+    console.log(theLettersThatMatchArray.join(" "));
 
-    // if it is not a letter then we will do nothing (maybe beep?)
-    if (!allTheValidGuesses.includes(theGuess) || theGuess.length !== 1) {
-        // beep or something?
-    } else {
-        theLettersGuessedArray.push(theGuess);
-        if (theWordToGuess.toLowerCase().includes(theGuess.toLowerCase())) {
-            // redo the display
-            for (i = 0; i < theWordToGuess.length; i++) {
-                // if theGuess is in theWordToGuessArray position[i] then splice
-                if (theWordToGuessArray[i].toLowerCase() == theGuess.toLowerCase()) {
-                    //check if the letter in theWordToGuessArray is capital and capitalize the spice if so
-                    if (theWordToGuessArray[i] === theGuess.toLowerCase()) {
-                        theLettersThatMatchArray.splice(i, 1, theGuess.toLowerCase());
-                    } else {
-                        theLettersThatMatchArray.splice(i, 1, theGuess.toUpperCase());
-                    }
-
-                    // theLettersThatMatchArray.splice(i, 1, theGuess);
-                }
-            }
-            // then see if the word has been completed
-            if (!theLettersThatMatchArray.includes("_")) {
-                theFinalMessage = ("You won! You guessed '" + theLettersThatMatchArray.join("") + "'. Reload this page to play again!");
-                alert(theFinalMessage);
-                break;
-            }
-            console.log(theLettersThatMatchArray.join(" "));
-            //if the guess is wrong the we decrement the guesses remaining
-        } else {
-            theGuessesRemaining = theGuessesRemaining - 1;
+    while (theGuessesRemaining > 0) {
+        theGuess = (prompt("Fill in the blanks to guess this " + theWordToGuess.length + " letter name: " + theLettersThatMatchArray.join(" ") + "\nYou have guessed these letters so far: " + theLettersGuessedArray.join(", ") + "\nYou have " + theGuessesRemaining + " guesses left. (Correct guesses don't count against you.)\nEnter your guess here and press [return]"));
+        if (theGuess == null) {
+            theMessage = "Reload this page to play again!";
+            document.getElementById("displayArea").innerHTML = theMessage;
+            break;
         }
-        console.log("Guesses remaining: " + theGuessesRemaining);
+
+        // if it is not a letter then we will do nothing (maybe beep?)
+        if (!allTheValidGuesses.includes(theGuess) || theGuess.length !== 1) {} else {
+            theLettersGuessedArray.push(theGuess);
+            if (theWordToGuess.toLowerCase().includes(theGuess.toLowerCase())) {
+                // redo the display
+                for (i = 0; i < theWordToGuess.length; i++) {
+                    // if theGuess is in theWordToGuessArray position[i] then splice
+                    if (theWordToGuessArray[i].toLowerCase() == theGuess.toLowerCase()) {
+                        //check if the letter in theWordToGuessArray is capital and capitalize the spice if so
+                        if (theWordToGuessArray[i] === theGuess.toLowerCase()) {
+                            theLettersThatMatchArray.splice(i, 1, theGuess.toLowerCase());
+                        } else {
+                            theLettersThatMatchArray.splice(i, 1, theGuess.toUpperCase());
+                        }
+                    }
+                }
+                // then see if the word has been completed
+                if (!theLettersThatMatchArray.includes("_")) {
+                    theWins = theWins + 1;
+                    theMessage = ("You won! You guessed '" + theLettersThatMatchArray.join("") + "'.\nYou have won " + theWins + " games so far.");
+                    alert(theMessage);
+                    document.getElementById("displayArea").innerHTML = theMessage;
+                    theMessage = "Games won: " + theWins;
+                    document.getElementById("theWins").innerHTML = theMessage;
+                    break;
+                }
+                console.log(theLettersThatMatchArray.join(" "));
+                //if the guess is wrong the we decrement the guesses remaining
+            } else {
+                theGuessesRemaining = theGuessesRemaining - 1;
+                theMessage = "Guesses remaining: " + theGuessesRemaining;
+                document.getElementById("theGuessesRemaining").innerHTML = theMessage;
+            }
+        }
     }
+    if (theGuessesRemaining == 0) {
+        theMessage = ("You ran out of guesses. The correct answer was '" + theWordToGuess.split("").join(" ") + "'.\nYou have won " + theWins + " games so far.")
+        alert(theMessage);
+        document.getElementById("displayArea").innerHTML = theMessage;
+        theMessage = "Games won: " + theWins;
+        document.getElementById("theWins").innerHTML = theMessage;
+    }
+    console.log(theLettersGuessedArray.join(", "));
+    document.getElementById("displayArea").innerHTML = theMessage;
 }
-if (theGuessesRemaining == 0) {
-    theFinalMessage = ("You ran out of guesses. The correct answer was '" + theWordToGuess.split("").join(" ") + "'. Reload this page to play again!")
-    alert(theFinalMessage);
-}
-console.log(theLettersGuessedArray.join(", "));
-document.write(theFinalMessage);
